f1d5f1c532220948ea89cceba36597f46085c3ce
Asa LeHolland <asaleholland@gmail.com>
2022-01-03 02:58:10 -0800

    add main fileprocessing and readme

---
+++ transciptionTesting.py
@@ -0,0 +1,63 @@
+from transcribeSR import getHarvardAudioText
+
+from transcribeGCSTT import processAudioinfileGCSTT
+
+from runJiwer import determineAccuracy
+
+from processSampleText import processHarvardSentenceList
+
+from timeit import default_timer
+
+from pathlib import Path
+
+# set this to True to test one audio file (rather than all 20)
+IS_TESTING_ONE = True
+
+transcriptFilePaths = Path('transcriptions').glob('*.txt')
+audioFilePaths = Path('audiofiles').glob('*.wav')
+
+transcriptionFunctions = {
+    "SpeechRecognition.recognize_google": getHarvardAudioText,
+    "google-cloud-speech": processAudioinfileGCSTT
+    }
+
+
+
+# create testing list of lists using audio files and transcriptions
+sample_data = []
+
+for audioFilePath in audioFilePaths:
+    sample_data.append([str(audioFilePath)])
+
+i = 0
+for transcriptFilePath in transcriptFilePaths:
+    sample_data[i].append(str(transcriptFilePath))
+    i += 1
+        
+
+if IS_TESTING_ONE:
+    sample_data = [sample_data[0]]
+
+
+# iterate over each transcription service function
+for title, transcriptionFunction in transcriptionFunctions.items():
+
+    # for each sample case
+    for audioPath, rawTranscriptPath in sample_data:
+
+        # obtain the expected final transcript result
+        expectedTranscription = processHarvardSentenceList(rawTranscriptPath)
+
+        # record the time taken to make the service call
+        start = default_timer()
+        actualTranscription = transcriptionFunction(audioPath)
+        end = default_timer()
+        duration = end - start
+
+        # calcualte accuracy of result against expected results
+        accuracy = determineAccuracy(expectedTranscription, actualTranscription)
+
+        if IS_TESTING_ONE: 
+            print(actualTranscription)
+
+    print(title, duration, accuracy)
