0c598c03d9242851df2778f37a5eec7a765970ab
Asa LeHolland <asaleholland@gmail.com>
2022-01-09 15:16:50 -0800

    got vosk transcription working

--- transcribeVosk.py
+++ transcribeVosk.py
@@ -2,10 +2,25 @@ from vosk import Model, KaldiRecognizer, SetLogLevel
 import wave
 import json
 import pickle
+from os.path import exists
 
-#  on startup, check to see if we have already iniitalized an NLP model of the audio data
-model = Model("./dependencies/vosk-model-en-us-0.22")
+print('starting Vosk...')
+#  on startup, check to see if we have a base pickle we can use to load the vosk model
+if not exists('voskModel.pickle'):
+    # Note that this is where I installed my copy of the Vosk training data, YMMV
+    model = Model("./dependencies/vosk-model-en-us-0.22")
+    with open('voskModel.pickle', 'wb') as f:
+        pickle.dump(model, f)
 
+# Once we have a working pickle, initialize the NLP model of the audio data
+with open('voskModel.pickle', 'rb') as f:
+    try:
+        model = pickle.load(f)
+    except EOFError:
+        print('Error: voskModel.pickle was empty')
+        model = Model("./dependencies/vosk-model-en-us-0.22")
+        
+print('loaded Vosk.')
 
 def transcribeVosk(infile):
     wf = wave.open(infile, "rb")
@@ -28,3 +43,7 @@ def transcribeVosk(infile):
 
     transcription_text = ' '.join(transcription)
     return transcription_text
+
+
+if __name__ == "__main__":
+    print(transcribeVosk('audio_files_harvard.wav'))
